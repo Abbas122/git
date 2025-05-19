bff399b22811bd33a52fd42c32405dd2279c9d02
Abbi <cusit313#gmial.com>
2025-05-19 13:49:35 +0500

    add the last file

--- dev-file.py
+++ dev-file.py
@@ -1,1 +1,12 @@
-print("This is file which i created in dev branch")
+list_of_frout=[1,2,3,4,"abc",True,False]
+print(type(list_of_frout))
+print(list_of_frout)
+list_of_names =list()
+print(type(list_of_names))
+list_of_players = []
+list_of_players.append("virat")
+list_of_players.append("seraj")
+list_of_players.append("afredi")
+print(list_of_players)
+list_of_players.clear()
+print(list_of_players)
